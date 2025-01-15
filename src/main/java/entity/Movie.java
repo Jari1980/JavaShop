e80792ed7bfaa6a521235174b557b820c345cb89
Jari Tenhunen <jari.tenhunen@outlook.com>
2025-01-15 14:24:23 +0100

    Category and Movie entity with Lombok added, application properties set to local MySQL server with hidden credentials

---
+++ src/main/java/entity/Movie.java
@@ -0,0 +1,22 @@
+package entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.Setter;
+
+import java.net.URL;
+@Data
+@Entity
+public class Movie {
+    @Setter(AccessLevel.NONE)
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private int id;
+    private String title;
+    private Category category;
+    private double price;
+    private int productionYear;
+    private String description;
+    private URL image;
+}
