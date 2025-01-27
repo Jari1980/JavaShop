22403921f95ef778681d99b8adae6b448094c353
Jari Tenhunen <jari.tenhunen@outlook.com>
2025-01-27 13:02:27 +0100

    Delete categories ok, returning view if category is under use of any movie

--- src/main/java/org/gr2/javashop/repository/MovieRepository.java
+++ src/main/java/org/gr2/javashop/repository/MovieRepository.java
@@ -1,10 +1,13 @@
 package org.gr2.javashop.repository;
 
+import org.gr2.javashop.entity.Category;
 import org.gr2.javashop.entity.Movie;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface MovieRepository extends CrudRepository<Movie, Integer> {
-
+    List<Movie> findByCategory(Category category);
 }
